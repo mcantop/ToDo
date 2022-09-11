e71867977029963b7132a7465074116fccf939c7
mcantop <maciejcantop@icloud.com>
2022-09-11 20:25:43 +0200

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Maciej on 11/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
